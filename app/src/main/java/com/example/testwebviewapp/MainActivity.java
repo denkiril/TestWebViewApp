8b2439fa4d4322f518f8b5bb4c61046b1efc59d3
Denis Kirilyuk <denkiril@gmail.com>
2022-11-14 02:08:20 +0300

    upd dependencies

--- app/src/main/java/com/example/testwebviewapp/MainActivity.java
+++ app/src/main/java/com/example/testwebviewapp/MainActivity.java
@@ -29,11 +29,10 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
         String url = "https://excurs-orel.ru/guidebook/";
-        url = "http://internal.tvbit.co/public/preview.html?sid=1-ffb0da6d25ef41f3c9c809ce38c83d70841e424fedc18291&account_id=1&template_id=693&broadcasting_mode=template&app_type=browser&socket_server=ws%3A%2F%2Finternal.tvbit.co%3A8185%2Fws&title=%D0%9F%D1%80%D0%B5%D0%B4%D0%BF%D1%80%D0%BE%D1%81%D0%BC%D0%BE%D1%82%D1%80%20%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%D0%B0%3A%20video%2019-10-2022&cache_mode=no_cache&language=ru";
         webView.loadUrl(url);
 
         WebViewClient webViewClient = new WebViewClient() {
-            @SuppressWarnings("deprecation") @Override
+            @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
@@ -48,4 +47,5 @@ public class MainActivity extends AppCompatActivity {
 
         webView.setWebViewClient(webViewClient);
     }
+
 }
